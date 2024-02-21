2c935653cd275e15a62d1b25f08026ca5c3eaeb8
Mark Lewis <mlewis89@gmail.com>
2024-02-21 19:43:41 +1100

    part function delete cal

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,28 +1,48 @@
-const notes = require('express').Router();
-const fs = require('fs');
-const {readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
+const notes = require("express").Router();
+const fs = require("fs");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
 
-notes.get('/', (req, res) =>
-{
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
-notes.post('/', (req,res)=> {
-    console.log(req.body);
-    const {title,text} = req.body;
-    if(req.body)
-    {
-       const newNote = {title, text, id: uuid()}; 
-       readAndAppend(newNote,'./db/db.json');
-       res.json('note added succesfully');
-    }
-    else{
-        res.errored('error in adding note');
-    }
-    
-})
+notes.post("/", (req, res) => {
+  console.log(req.body);
+  const { title, text } = req.body;
+  if (req.body) {
+    const newNote = { title, text, id: uuid() };
+    readAndAppend(newNote, "./db/db.json");
+    res.json("note added succesfully");
+  } else {
+    res.json("error in adding note");
+  }
+});
 
+notes.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  if (id !== 'undefined') {
+    const d = [];
+    readFromFile("./db/db.json")
+      .then((data) => JSON.parse(data))
+      .then((data) => {
+        return data.filter((x) => x.id !== id);
+      })
+      .then((data) => {
+        console.log("saving to file", data);
+        //writeToFile('./db/db.json',data);
+        //res.json(JSON.parse(data));
+      });
+  }
+  else
+  {
+    res.json("No ID received");
+    console.log('no note id')
+  }
+});
 
-
-module.exports =  notes;
+module.exports = notes;
