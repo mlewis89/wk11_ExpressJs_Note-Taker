577cb0b55f2a8d665cbfa1c1e1814e25e1b57302
Mark Lewis <mlewis89@gmail.com>
2024-02-25 15:55:59 +1100

    added comments to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,30 @@
+//include external libraries
 const express = require('express');
 const path = require('path');
+//create link to routing file
 const api =  require('./routes/index.js');
 
+//define port for use
 const PORT =  process.env.port || 3001;
-
+//create instance of express
 const app = express();
 
+//Add in middle wear 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//create /api route
 app.use('/api',api);
 
+//create route to serve up public html documents
 app.use(express.static('public'));
 
+//create route for /notes to to serve up note page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname,'/public/notes.html'))
 });
  
+//start the app on the specified prt
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
