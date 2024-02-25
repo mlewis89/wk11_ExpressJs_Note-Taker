1300943e3dfe0cd6741b50d3cbcaf0602cd38769
Mark Lewis <mlewis89@gmail.com>
2024-02-25 16:13:28 +1100

    added comments to routes

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,7 +1,8 @@
+//create instanceof express router
 const router = require('express').Router();
-
+//configure route for /api/notes
 const notesRouter = require('./notes');
-
+//create instance of route
 router.use('/notes', notesRouter);
-
+//export for extnal use
 module.exports = router
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,5 +1,8 @@
+//include express router
 const notes = require("express").Router();
+//include file system
 const fs = require("fs");
+//include helpers
 const {
   readFromFile,
   readAndAppend,
@@ -7,24 +10,32 @@ const {
 } = require("../helpers/fsUtils");
 const uuid = require("../helpers/uuid");
 
+//GET method to return current notes in the 'database'
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+//POST methood to add a new note to the 'database'
 notes.post("/", (req, res) => {
   console.log(req.body);
-  const { title, text } = req.body;
+  //check the body of the request is not empty
   if (req.body) {
+    const { title, text } = req.body;
+    //create new note object.
     const newNote = { title, text, id: uuid() };
+    //append new note to 'database' file
     readAndAppend(newNote, "./db/db.json");
     res.json("note added succesfully");
   } else {
-    res.json("error in adding note");
+    res.json("error: body required for POST method");
   }
 });
 
+//DELETE method - to delete a note form the 'database' file
 notes.delete("/:id", (req, res) => {
+  //save the data within the path paramether to variable 'id'
   const id = req.params.id;
+  //check that id passed, in not empty
   if (id !== null && id !== undefined) {
     const d = [];
     readFromFile("./db/db.json")
@@ -33,9 +44,18 @@ notes.delete("/:id", (req, res) => {
         return data.filter((x) => x.id !== id);
       })
       .then((data) => {
-        console.log("saving to file", data);
-        writeToFile('./db/db.json',data);
-        res.json(data);
+        //if new data leaves the array empty
+        if(data.length > 0)
+        {
+          console.log("saving to file", data);
+          writeToFile('./db/db.json',data);
+          res.json(data);
+        }else
+        {
+          data = [];
+          writeToFile('./db/db.json',data);
+          res.json(data);
+        }
       })
       .catch((error)=>res.json(error));
   }
@@ -45,5 +65,5 @@ notes.delete("/:id", (req, res) => {
     console.log('no note id')
   }
 });
-
+//export note routes for external use
 module.exports = notes;
