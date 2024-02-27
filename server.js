40df4fa1ab8b01417fe08baec216668448688e8d
Mark Lewis <mlewis89@gmail.com>
2024-02-27 14:30:46 +1100

    file restructure

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const api =  require('./routes/index.js');
 
 //define port for use
-const PORT =  process.env.port || 3001;
+const PORT =  process.env.PORT || 3001;
 //create instance of express
 const app = express();
 
